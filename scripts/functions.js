a33532648cac2b09d108a06b4e65511bd3bbef65
SPavelV <pashius2010@gmail.com>
2018-06-29 10:41:38 +0400

    rest parametr

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,29 +1,48 @@
-function makeRequest(url, timeout, callback) {
-  timeout = timeout || 2000;
-  callback = callback || function () {};
-}
+// function makeRequest(url, timeout, callback) {
+//   timeout = timeout || 2000;
+//   callback = callback || function () {};
+// }
+//
+// function makeRequest2(url, timeout, callback) {
+//   timeout = (typeof timeout == 'undefined') ? 2000 : timeout;
+//   callback = (typeof callback == 'undefined') ? function(){} : callback ;
+// }
+//
+// function pick(object) {
+//   let result = Object.create(null);
+//
+//   for(let i = 1, len = arguments.length; i < len; i++) {
+//     result[arguments[i]] =  object[arguments[i]];
+//   }
+//
+//   return result;
+// }
+//
+// let book = {
+//   title: 'Understanding ECMAScript 6',
+//   author: 'Nicolas C.Zakas',
+//   year: 2016
+// };
+//
+// let bookData = pick(book, 'author', 'year');
+// console.log(bookData.author);
+// console.log(bookData.year);
 
-function makeRequest2(url, timeout, callback) {
-  timeout = (typeof timeout == 'undefined') ? 2000 : timeout;
-  callback = (typeof callback == 'undefined') ? function(){} : callback ;
-}
 
-function pick(object) {
+function pick2(object, ...keys) {
   let result = Object.create(null);
 
-  for(let i = 1, len = arguments.length; i < len; i++) {
-    result[arguments[i]] =  object[arguments[i]];
+  for (let i = 0, len = keys.length; i < len; i++) {
+    result[keys[i]] = object[keys[i]];
   }
 
   return result;
 }
 
-let book = {
-  title: 'Understanding ECMAScript 6',
-  author: 'Nicolas C.Zakas',
-  year: 2016
-};
-
-let bookData = pick(book, 'author', 'year');
-console.log(bookData.author);
-console.log(bookData.year);
+function checkArgs(...args) {
+  console.log(args.length);
+  console.log(arguments.length);
+  console.log(args[0], arguments[0]);
+  console.log(args[1], arguments[1]);
+}
+checkArgs('a', 'b');
