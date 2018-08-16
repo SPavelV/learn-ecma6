4f903d8a71430aa0dec414c0cef8ed272e45e47a
SPavelV <pashius2010@gmail.com>
2018-08-16 10:47:17 +0400

    arrow functions

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,3 +1,4 @@
+'use strict';
 // function makeRequest(url, timeout, callback) {
 //   timeout = timeout || 2000;
 //   callback = callback || function () {};
@@ -29,20 +30,130 @@
 // console.log(bookData.year);
 
 
-function pick2(object, ...keys) {
-  let result = Object.create(null);
+// function pick2(object, ...keys) {
+//   let result = Object.create(null);
+//
+//   for (let i = 0, len = keys.length; i < len; i++) {
+//     result[keys[i]] = object[keys[i]];
+//   }
+//
+//   return result;
+// }
+//
+// function checkArgs(...args) {
+//   console.log(args.length);
+//   console.log(arguments.length);
+//   console.log(args[0], arguments[0]);
+//   console.log(args[1], arguments[1]);
+// }
+// checkArgs('a', 'b');
 
-  for (let i = 0, len = keys.length; i < len; i++) {
-    result[keys[i]] = object[keys[i]];
+// var add = new Function('first', 'second = first', 'return first + second');
+// console.log(add(1,1));
+// console.log(add(1));
+//
+// var pickFirst = new Function('...args', 'return args[0]');
+// console.log(pickFirst(1,2));
+
+// spread
+
+// let value1 = 25,
+//     value2 = 50;
+// console.log(Math.max(value1, value2));
+//
+// let values = [25, 50, 75, 100];
+// console.log(Math.max.apply(Math, values));
+// console.log(Math.max(...values));
+//
+// let values2 = [-25, -50, -75, -100];
+// console.log(Math.max(...values2, 0));
+
+
+// determine how to call a function
+
+// function Person(name) {
+//   if(this instanceof Person) {
+//     this.name = name;
+//   } else  {
+//     throw new Error("You must use new with Person.")
+//   }
+// }
+//
+// let person = new Person("Dan");
+// // let notAPerson = Person("Dan");
+//
+// let notAPerson = Person.call(person, "Vova");
+
+// meta prop new.target
+// function Person(name) {
+//   if(typeof new.target !== 'undefined') {
+//     this.name = name;
+//   } else {
+//     throw new Error('You must use new with Person');
+//   }
+// }
+// let person = new Person("Dan");
+// let notAPerson = Person.call(person, "Vova");
+
+// function Person(name) {
+//   if(typeof new.target === Person) {
+//     this.name = name;
+//   } else {
+//     throw new Error('You must use new with Person');
+//   }
+// }
+// function AnoterPerson(name) {
+//   Person.call(this, name);
+// }
+//
+// let person = new Person('Bob');
+// let anotherPerson = new AnoterPerson('Bob');
+
+
+// arrow functions:
+
+// - IIFE
+
+// let person = function(name) {
+//   return {
+//     getName: function () {
+//       return name;
+//     }
+//   };
+// }('Nicolas');
+// console.log(person.getName());
+//
+// let person2 = ( (name) => {
+//   return {
+//     getName: function () {
+//       return name;
+//     }
+//   };
+// })('Nicolas2');
+// console.log(person2.getName());
+
+// - Lack of biding this
+
+let PageHandler = {
+  id: "123456",
+  init: function () {
+    document.addEventListener('click', function (e) {
+      this.doSomething(e.type);
+    }.bind(this), false);
+  },
+
+  doSomething: function (type) {
+    console.log('Handling ' + type + ' for ' + this.id);
   }
+};
 
-  return result;
-}
-
-function checkArgs(...args) {
-  console.log(args.length);
-  console.log(arguments.length);
-  console.log(args[0], arguments[0]);
-  console.log(args[1], arguments[1]);
-}
-checkArgs('a', 'b');
+let PageHandler2 = {
+  id: '123456',
+  init: function () {
+    document.addEventListener('click',
+              event => this.doSomething(event.type), false);
+  },
+  doSomething: function (type) {
+    console.log('Handing ' + type + ' for ' + this.id);
+  }
+};
